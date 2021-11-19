ade9f470bdd61f55670a2f5374e96b1e7c127a1a
walterYangana <medinawalter3@gmail.com>
2021-11-19 16:34:15 -0500

    subiendo sistema de gestion de proyectos

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='bg-red-400 text-black text-6xl'>Hola Equipo Turing, este proyecto ya está en react y tiene tailwind, no olviden hacer yarn install 
+        despues de bajar el proyecto remoto a local
+      </div>
+
+    </div>   
+    
+  );
+}
+
+export default App;
